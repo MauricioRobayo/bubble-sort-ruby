68259c1c4e8b30351a1d7d67cd5cf9e1efce3bd0
Mauricio Robayo <rfmajo@gmail.com>
2019-08-14 14:38:36 -0500

    Add bubble_sort method

---
+++ buble-sort.rb
@@ -0,0 +1,15 @@
+def bubble_sort(array)
+  (0...array.length - 1).each do |i|
+    sorted = true
+    (0...array.length - 1 - i).each do |j|
+      if array[j] > array[j + 1]
+        array[j], array[j + 1] = array[j + 1], array[j]
+        sorted = false
+      end
+    end
+    break if sorted
+  end
+  return array
+end
+
+print bubble_sort([4,3,78,2,0,2])
