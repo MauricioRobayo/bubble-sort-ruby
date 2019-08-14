0fa5bc92054ace1b839e75535ef45157280ec868
Nicolás Mariño <nicolasmarinoparra@gmail.com>
2019-08-14 16:43:45 -0500

    added new method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -15,3 +15,21 @@ def bubble_sort(array)
 end
 
 print bubble_sort([4, 3, 78, 2, 0, 2])
+
+def bubble_sort_by(array)
+  (0...array.length - 1).each do |i|
+    sorted = true
+    (0...array.length - 1 - i).each do |j|
+      puts yield(array[j], array[j + 1])
+      if array[j] > array[j + 1]
+        array[j], array[j + 1] = array[j + 1], array[j]
+        sorted = false
+      end
+    end
+    break if sorted
+  end
+  array
+end
+bubble_sort_by(%w[hi hello hey]) do |left, right|
+  left.length - right.length
+end
